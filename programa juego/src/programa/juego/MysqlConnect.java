f87bf63026e3b8f31acf39a52d575d61fb871aa1
veronica200816 <veronicaagonzz@gmail.com>
2016-11-16 12:15:10 -0600

    Juego
    Juegoooo

---
+++ programa juego/src/programa/juego/MysqlConnect.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programa.juego;
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author acer
+ */
+public class MysqlConnect {
+     Connection conn=null;
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/datos","root","vero");
+            JOptionPane.showMessageDialog(null,"conected to database");
+            return conn;
+        }
+        
+    catch(Exception e){
+    JOptionPane.showMessageDialog(null,e);
+    return null;
+}
+    
+    }
+}
---
+++ programa juego/src/programa/juego/ProgramaJuego.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programa.juego;
+
+
+
+/**
+ *
+ * @author acer
+ */
+public class ProgramaJuego {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new inter ().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
